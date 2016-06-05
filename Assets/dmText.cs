7b9ebf4bb397eeeef727f7f0b88f8683266c0851
Kapur <ankkapur@amazon.com>
2016-06-05 05:56:32 -0700

    Adjusted UI and canvas positions as per my Nexus display.

--- Assets/dmText.cs
+++ Assets/dmText.cs
@@ -2,8 +2,6 @@
 using UnityEngine.UI;
 using System.Collections;
 
-//#define MAX_NO 10
-
 public class dmText : MonoBehaviour {
 	Text text;
 
@@ -17,29 +15,15 @@ public class dmText : MonoBehaviour {
 	
 	// Update is called once per frame
 	public void Update () {
-//		GameObject[] objs = GameObject.FindGameObjectsWithTag ("furniture");
 		string selectedFurniture = global_stuff.selectedFurniture;
-
-
-			
-
-		/*
-		GameObject arCamera = GameObject.Find ("ARCamera");
-		Vector3 closestP = arCamera.GetComponent<Transform> ().position;
-		closestP.x =  0 - closestP.x;
-		closestP.y =  closestP.y;
-		text.transform.position = closestP;
-		*/
 
 		if (text != null) {
 			text.text = "";
 			int sumPrice = 0;
-//			foreach (GameObject obj in objs) {
 			if (selectedFurniture != null) {
 				GameObject obj = GameObject.Find (selectedFurniture);
 				
 				Vector3 vec = obj.GetComponent<Renderer> ().bounds.size;
-				string name = obj.name;
 				furnPrice price = obj.GetComponent<furnPrice> ();
 				sumPrice += price.price;
 				if (obj.activeSelf == true) {
@@ -47,7 +31,6 @@ public class dmText : MonoBehaviour {
 					text.text = text.text + "Dimensions: " + (int)vec.x + "\' x " + (int)vec.y + "\' x " + (int)vec.z + "\'\n";
 				}
 			}
-			//text.text = "Furniture details\n" + "Price: $" + sumPrice.ToString () + "\n" + text.text; 
 		}
 	}
 }
